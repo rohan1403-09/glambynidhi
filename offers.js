9d43be53ed94736ea386548d64bf8a5c397cdf80
rohan1403-09 <l9552134@gmail.com>
2026-08-03 10:11:58 +0530

    Update offers.js

--- offers.js
+++ offers.js
@@ -13,7 +13,7 @@ offerCards.forEach((card) => {
     card.classList.add("is-selected");
     chosenOfferName.textContent = offer;
     bookingLink.href = `index.html?offer=${encodeURIComponent(offer)}#booking`;
-    offerAndServiceLink.href = `index.html?offer=${encodeURIComponent(offer)}&both=true#booking`;
+    offerAndServiceLink.href = `index.html?offer=${encodeURIComponent(offer)}&both=true#services`;
     selectionPanel.hidden = false;
     selectionPanel.scrollIntoView({ behavior: "smooth", block: "nearest" });
   });
@@ -23,6 +23,6 @@ cancelSelection.addEventListener("click", () => {
   offerCards.forEach((card) => card.classList.remove("is-selected"));
   chosenOfferName.textContent = "";
   bookingLink.href = "index.html#booking";
-  offerAndServiceLink.href = "index.html#booking";
+  offerAndServiceLink.href = "index.html#services";
   selectionPanel.hidden = true;
 });
